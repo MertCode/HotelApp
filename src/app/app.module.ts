197356d6c8597d490c21e2a3e95716b4b9140198
Mert Sozen <mertcode0@gmail.com>
2024-01-25 20:54:04 +0100

    removed 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookingCalendarComponent } from './pages/booking-calendar/booking-calendar.component';
 import { BookingListComponent } from './pages/booking-list/booking-list.component';
 import { CustomerComponent } from './pages/customer-list/customer.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NewBookingComponent } from './pages/new-booking/new-booking.component';
 import { NewCustomerComponent } from './pages/new-customer/new-customer.component';
 import { RoomsComponent } from './pages/rooms/rooms.component';
 import { UsersComponent } from './pages/users/users.component';
-import { NewBookingComponent } from './pages/new-booking/new-booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BookingCalendarComponent,
     BookingListComponent,
-    DashboardComponent,
     LayoutComponent,
     RoomsComponent,
     UsersComponent,
     CustomerComponent,
     NewCustomerComponent,
-    NewBookingComponent
+    NewBookingComponent,
   ],
   imports: [
     BrowserModule,
